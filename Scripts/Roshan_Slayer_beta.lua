4656112c017da01a8e1cc66bfb0afb5be68436c0
Penis <ckatuhkoo@gmail.com>
2015-08-25 23:21:46 +0300

    Roshan_Slayer_beta.lua

--- Scripts/Roshan_Slayer_beta.lua
+++ Scripts/Roshan_Slayer_beta.lua
@@ -83,14 +83,14 @@ function Main(tick)
       Sleep(3000) return
     end
 
-    if Step == 1 and roshan and me:GetDistance2D(roshan) > 350 then
+    if Step == 1 and roshan and me:GetDistance2D(roshan) > 343 then
       me:AttackMove(Vector(4282,-1816,100))
       Step = 2
       Sleep(1700)
     elseif Step == 2 then
       me:Move(Vector(3478,-1969,100))
       Step = 3
-    elseif Step == 3 and roshan and me:GetDistance2D(roshan) < 295 and me:GetDistance2D(Vector(3478,-1969,100)) < 3 then
+    elseif Step == 3 and roshan and me:GetDistance2D(roshan) < 280 and me:GetDistance2D(Vector(3478,-1969,100)) < 2 then
       me:Attack(Ward[1])
       me:Stop()
       Step = 1
